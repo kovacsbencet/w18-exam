aea9b8331643acd556616485830cb389fb1b12a5
kovacsbencet <kovacsbencet@gmail.com>
2022-05-17 22:35:57 +0200

    Subscription working.

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -7,41 +7,35 @@ import Subscription from "./Subscription"
 function App (){
 
   const [loading, setLoading] = useState(false)
-
   const [data, setData] = useState([])
-
   const [subscription, setSubscription] = useState(false)
 
   async function fetchData(){
-
     const response = await fetch("https://demoapi.com/api/series/howimetyourmother")
-
     const responseJSON = await response.json()
-
-    console.log(responseJSON)
-
     setData(responseJSON)
     setLoading(false)
   }
 
-  useEffect(()=>{
+  useEffect(() =>{
     setLoading(true)
-    fetchData()
+    setTimeout(() => {fetchData()}, 5000)
+    setTimeout(() => {setSubscription(true)}, 10000)
   }, [])
-
-  setTimeout(() => {
-    setSubscription(true)
-  }, 10000);
 
   return (
     <div className="App">
       <h1>Series Api</h1>
-      {loading ? <LoadingMask/> : 
-          <>
-            {data.map(({name, details}) => <Character key={name} name={name} details={details}/>)}
-            {subscription ? <Subscription/> : ""}
-          </>
+      {
+      loading ? 
+      <LoadingMask/> : 
+      data.map(({name, details}) => <Character key={name} name={name} details={details}/>)
       }
+      {
+      subscription 
+      ? 
+      <Subscription/> :
+      ""}
     </div>
   );
 }
--- reactinitial/src/Subscription.jsx
+++ reactinitial/src/Subscription.jsx
@@ -1,35 +1,49 @@
 import React, {useState} from 'react'
+import LoadingMask from "./LoadingMask"
+
 
 function Subscription() {
 
     const [email, setEmail] = useState("");
+    const [loading, setLoading] = useState(false)
+    const [submitted, setSubmitted] = useState(false)
+    const [submitText, setSubmitText] = useState("")
 
-    const [loading, setLoading] = useState("")
+    const emailChange = e => setEmail(e.target.value);
 
-    const onEmailChange = e => setEmail(e.target.value);
+    const onSubmitEvents = () =>{
+        setSubmitText("Subscribed")
+        setSubmitted(true)
+        setLoading(false)
+    }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-    
-        const data = {email};
-        const requestOptions = {
+    const emailSubmit = () => {
+        setLoading(true)
+        const subscriptionData = {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data)
+          body: JSON.stringify({email})
         };
-        fetch("https://demoapi.com/api/series/newsletter", requestOptions)
-          .then(response => response.json())
-          .then(res => console.log(res));
-      };
+        fetch("https://demoapi.com/api/series/newsletter", subscriptionData)
+        .then(response => response.json())
+        .then(response => 
+        (response.done === true) ?
+        onSubmitEvents() : console.log("ÉC"))
+ 
+    };
 
     return (
         <div>
-            <h2>Subscribe to our newsletter.</h2>
-            {<form>
-                <input type="email" value={email} onChange={onEmailChange}/>
-                <button onClick={handleSubmit}>Subscribe!</button>
-            </form>}
-            <h3>{loading}</h3>
+            {
+            loading ?
+            <LoadingMask /> : null}
+            {submitted ? <div>Subscribed</div> :
+            <>
+                <h2>Subscribe to our newsletter.</h2>
+                <input type="email" value={email} onChange={emailChange}/>
+                <button disabled={email.includes("@" && ".") ? false : true} onClick={emailSubmit}>Subscribe!</button>
+            </>
+            }
         </div>
     )
 }
